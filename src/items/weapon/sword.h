3e9927c8b2e9e40461ac13d607704b940151d7c9
trrpy3 <alexxxxx142007@gmail.com>
2026-03-13 01:39:25 +0300

    Update sword.h

--- src/items/weapon/sword.h
+++ src/items/weapon/sword.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "weapon.h"
-
+#include <string>
+#include "enums.h"
 class Sword : public Weapon {
-    // специфичные поля для меча можно добавить сюда
+public:
+    Sword(const std::string& name, int damage, float accuracy, int price = 0, int minLevel = 1);
+    std::string getTypeName() const override;
 };
