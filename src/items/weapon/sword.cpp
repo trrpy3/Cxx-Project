cf4b9cb15313dda6b6e106c2c434b0a9291627a5
trrpy3 <alexxxxx1422007@gmail.com>
2026-03-13 01:36:12 +0300

    sword added

---
+++ src/items/weapon/sword.cpp
@@ -0,0 +1,4 @@
+Sword::Sword(const std::string& name, int damage, float accuracy, int price, int minLevel)
+    : Weapon(name, damage, accuracy, damage_Type::melee, price, minLevel) {
+}
+std::string Sword::getTypeName() const { return "Sword"; }
