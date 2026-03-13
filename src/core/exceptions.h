fee6e0fbaa519911581e2e01f8542f5885c39282
GUzLlK <vectorostapenko777@gmail.com>
2026-03-13 13:11:49 +0300

    updating exceptions

---
+++ src/core/exceptions.h
@@ -0,0 +1,16 @@
+// exceptions.h
+#pragma once
+#include <stdexcept>
+#include <string>
+class GameException : public std::runtime_error {
+public:
+    explicit GameException(const std::string& msg) : std::runtime_error(msg) {}
+};
+class FileException : public GameException {
+public:
+    explicit FileException(const std::string& msg) : GameException("File error: " + msg) {}
+};
+class InventoryException : public GameException {
+public:
+    explicit InventoryException(const std::string& msg) : GameException("Inventory error: " + msg) {}
+};
