adbcf38d1d9b4d8c177a37c6732bb4bddab3e83c
TaleDras <yarwrx@mail.ru>
2026-03-13 13:26:06 +0300

    armor has been added

---
+++ src/items/armour/armor.cpp
@@ -0,0 +1,18 @@
+#include "armor.h"
+
+Armor::Armor(const std::string& name, int defense, int price, int minLevel)
+    : name(name), defense(defense), price(price), minLevel(minLevel) {
+}
+int Armor::getDefense() const { return defense; }
+std::string Armor::getName() const { return name; }
+int Armor::getPrice() const { return price; }
+int Armor::getMinLevel() const { return minLevel; }
+Helmet::Helmet(const std::string& name, int defense, int projectile_defense, int price, int minLevel)
+    : Armor(name, defense, price, minLevel), projectile_defense(projectile_defense) {
+}
+int Helmet::getProjectileDefense() const { return projectile_defense; }
+std::string Helmet::getType() const { return "Helmet"; }
+Chestplate::Chestplate(const std::string& name, int defense, int price, int minLevel)
+    : Armor(name, defense, price, minLevel) {
+}
+std::string Chestplate::getType() const { return "Chestplate"; }
--- src/items/armour/armor.h
+++ src/items/armour/armor.h
@@ -1,9 +1,31 @@
 #pragma once
 
 #include <string>
-
 class Armor {
 public:
+    Armor(const std::string& name, int defense, int price = 0, int minLevel = 1);
+    virtual ~Armor() = default;
+    int getDefense() const;
+    std::string getName() const;
+    int getPrice() const;
+    int getMinLevel() const;
+    virtual std::string getType() const = 0;
+protected:
     std::string name;
-    int defense; 
+    int defense;
+    int price;
+    int minLevel;
+};
+class Helmet : public Armor {
+public:
+    Helmet(const std::string& name, int defense, int projectile_defense, int price = 0, int minLevel = 1);
+    int getProjectileDefense() const;
+    std::string getType() const override;
+private:
+    int projectile_defense;
+};
+class Chestplate : public Armor {
+public:
+    Chestplate(const std::string& name, int defense, int price = 0, int minLevel = 1);
+    std::string getType() const override;
 };
