5f12cc0b7ae2ff59856104d7b2bd8792f4eae916
TaleDras <yarwrx@mail.ru>
2026-03-13 11:02:08 +0300

    the first part of the shop has been added

---
+++ src/core/shop.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "location.h"
+#include <vector>
+#include <memory>
+#include "weapon.h"
+#include "armor.h"
+#include "healthpotion.h"
+class Shop : public Location {
+public:
+    Shop(const std::string& name);
+    void addWeapon(std::unique_ptr<Weapon> item);
+    void addArmor(std::unique_ptr<Armor> item);
+    void addPotion(std::unique_ptr<HealthPotion> item);
+    void enter(Player& player) override;
+private:
+    std::vector<std::unique_ptr<Weapon>> allWeapons;
+    std::vector<std::unique_ptr<Armor>> allArmors;
+    std::vector<std::unique_ptr<HealthPotion>> allPotions;
+};
